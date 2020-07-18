9ea5e2377216572f3c4e0ef620e78bb6929a9309
Michael Sedrak <m.sedrak@live.com>
2020-07-18 18:38:21 +0200

    Wrapper functionality added to ARAP_refactor

--- ARAP/include/InteractiveARAP.h
+++ ARAP/include/InteractiveARAP.h
@@ -1,19 +1,35 @@
 #pragma once
 
-#include <class1.h>
+#include <mesh.h>
+#include <arap.h>
 
 namespace InteractiveARAP
 {
-	class ARAP
+	class NativeInterface
 	{
 	private:
-		static dummyClass* engine;
+		std::vector<Mesh> meshes;
+		ARAP arapEngine;
+		
+	public:
+		Mesh deformedMesh;
+		NativeInterface();
+		void LoadAllMeshes();
+		void Process(int size, float* constraints, unsigned int nIter = 10);
+		void SetBaseMesh(int index);
+		const std::vector<float>& getMeshVertices(int index);
+		const std::vector<int>& getMeshIndices(int index);
+	};
+
+	class ARAPWrapper
+	{
+	private:
+		static NativeInterface* engine;
 
 	public:
-		ARAP();
-		~ARAP();
-		void Process();
-		static dummyClass* GetInstance();
+		ARAPWrapper();
+		~ARAPWrapper();
+		static NativeInterface* GetInstance();
 		static void Destroy();
 
 	};
@@ -24,27 +40,58 @@ namespace InteractiveARAP
 	{
 		EXPORT_API void Initialize()
 		{
-			ARAP::GetInstance();
+			ARAPWrapper::GetInstance();
+			ARAPWrapper::GetInstance()->LoadAllMeshes();
 		}
 
-		EXPORT_API float* GetVertices()
+		EXPORT_API const float* GetVertices(int modelIndex)
 		{
-			return ARAP::GetInstance()->GetVertices().data();
+			return ARAPWrapper::GetInstance()->getMeshVertices(modelIndex).data();
 		}
 
-		EXPORT_API int* GetIndices()
+		EXPORT_API const int* GetIndices(int modelIndex)
 		{
-			return ARAP::GetInstance()->GetIndices().data();
+			return ARAPWrapper::GetInstance()->getMeshIndices(modelIndex).data();
 		}
 
-		EXPORT_API int GetIndicesSize()
+		EXPORT_API int GetIndicesSize(int modelIndex)
 		{
-			return ARAP::GetInstance()->GetIndices().size();
+			return ARAPWrapper::GetInstance()->getMeshIndices(modelIndex).size();
 		}
 
-		EXPORT_API int GetVerticesSize()
+		EXPORT_API int GetVerticesSize(int modelIndex)
 		{
-			return ARAP::GetInstance()->GetVertices().size();
+			return ARAPWrapper::GetInstance()->getMeshVertices(modelIndex).size();
+		}
+
+		EXPORT_API const float* GetDeformedVertices()
+		{
+			return ARAPWrapper::GetInstance()->deformedMesh.getVertices().data();
+		}
+
+		EXPORT_API const int* GetDeformedIndices()
+		{
+			return ARAPWrapper::GetInstance()->deformedMesh.getTriangles().data();
+		}
+
+		EXPORT_API int GetDeformedIndicesSize()
+		{
+			return ARAPWrapper::GetInstance()->deformedMesh.getTriangles().size();
+		}
+
+		EXPORT_API int GetDeformedVerticesSize()
+		{
+			return ARAPWrapper::GetInstance()->deformedMesh.getVertices().size();
+		}
+
+		EXPORT_API void Process(int size, float* constraints, unsigned int nIter = 10)
+		{
+			ARAPWrapper::GetInstance()->Process(size,constraints,nIter);
+		}
+
+		EXPORT_API void SetBaseMesh(int meshIdx)
+		{
+			ARAPWrapper::GetInstance()->SetBaseMesh(meshIdx);
 		}
 	}
 
--- ARAP/src/InteractiveARAP.cpp
+++ ARAP/src/InteractiveARAP.cpp
@@ -2,32 +2,76 @@
 
 namespace InteractiveARAP
 {
-	dummyClass* ARAP::engine;
+	NativeInterface* ARAPWrapper::engine;
 
-	ARAP::ARAP()
+	ARAPWrapper::ARAPWrapper()
 	{
 	}
 
-	ARAP::~ARAP()
+	ARAPWrapper::~ARAPWrapper()
 	{	
 	}
 
-	void ARAP::Process()
-	{
-		
-	}
-
-	dummyClass* ARAP::GetInstance()
+	NativeInterface* ARAPWrapper::GetInstance()
 	{
 		if (!engine)
-			engine = new dummyClass();
+			engine = new NativeInterface();
 		return engine;
 	}
 
-	void ARAP::Destroy()
+	void ARAPWrapper::Destroy()
 	{
 		if (engine)
 			delete engine;
 		engine = nullptr;
+	}
+
+	NativeInterface::NativeInterface()
+	{
+		meshes.resize(11);
+	}
+	
+	void NativeInterface::LoadAllMeshes()
+	{
+		meshes[0].loadMesh("Assets/meshes/armadillo_1k.off");
+		meshes[1].loadMesh("Assets/meshes/bar1.off");
+		meshes[2].loadMesh("Assets/meshes/bar2.off");
+		meshes[3].loadMesh("Assets/meshes/bar2.off");
+		meshes[4].loadMesh("Assets/meshes/bar3.off");
+		meshes[5].loadMesh("Assets/meshes/cactus_highres.off");
+		meshes[6].loadMesh("Assets/meshes/cactus_small.off");
+		meshes[7].loadMesh("Assets/meshes/cylinder_small.off");
+		meshes[8].loadMesh("Assets/meshes/dino.off");
+		meshes[9].loadMesh("Assets/meshes/square_21.off");
+		meshes[10].loadMesh("Assets/meshes/square_21_spikes.off");
+	}
+
+	void NativeInterface::Process(int size, float* rawconstraints, unsigned int nIter)
+	{
+		std::vector<std::pair<unsigned int, Eigen::Vector3f>> constraints;
+		for (int i = 0; i < size; i += 4)
+		{
+			unsigned int vertexIndex = (unsigned int)rawconstraints[i];
+			Eigen::Vector3f vertexConstraint = Eigen::Vector3f(rawconstraints[i + 1], rawconstraints[i + 2], rawconstraints[i + 3]);
+			constraints.push_back(std::pair<unsigned int, Eigen::Vector3f>(vertexIndex, vertexConstraint));
+		}
+
+		arapEngine.DeformMesh(constraints, nIter);
+		deformedMesh = arapEngine.GetDeformedMesh();
+	}
+
+	void NativeInterface::SetBaseMesh(int index)
+	{
+		arapEngine.SetBaseMesh(meshes[index]);
+	}
+	
+	const std::vector<float>& NativeInterface::getMeshVertices(int index)
+	{
+		return meshes[index].getVertices();
+	}
+
+	const std::vector<int>& NativeInterface::getMeshIndices(int index)
+	{
+		return meshes[index].getTriangles();
 	}
 }
